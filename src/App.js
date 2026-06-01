b954db19988b76f7772b6d1e323e147018d0c50c
NaingHtut0o <nainghtutoo.life@gmail.com>
2026-06-01 14:33:29 +0630

    Try Some Functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Item = ({name, price}) => (
+	<li>{name}, ${price}</li>
+	);
+	
+const AddForm = ({add}) => {
+	let nameRef = useRef();
+	let priceRef = useRef();
+	
+	let addItem = () =>	{
+		let name = nameRef.current.value;
+		let price = priceRef.current.value;
+		
+		add(name, price);
+	}
+	
+	return (
+		<div>
+			<input type="text" ref={nameRef}/><br/>
+			<input type="price" ref={priceRef}/><br/>
+			<button onClick={addItem}>Add</button>
+		</div>
+	);
+}
+
+const App = props => {
+	let [items, setItems] = useState([
+			{ id: 1, name: 'Apple', price: 0.99 },
+			{ id: 2, name: 'Orange', price: 1.49 },
+		]);
+		
+	const add = (name, price) => {
+		let id = items.length +1;
+		
+		setItems([
+			...items,
+			{ id, name, price },
+		]);
+	}
+	
+	return (
+		<div>
+			<ul>
+				{items.map( i => <Item key={i.id} name={i.name} price={i.price}/>)}
+			</ul>
+			<AddForm add={add}/>
+		</div>
+	)
 }
 
 export default App;
